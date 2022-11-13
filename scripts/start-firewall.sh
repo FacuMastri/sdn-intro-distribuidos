f5d999f7c36f8d67a34b0bd59e4d377ea153d658
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-11-12 23:27:51 -0300

    Agrego script

---
+++ scripts/start-firewall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Start the firewall
+# Usage: start-firewall.sh <switch-id> <rules-path>
+
+SWITCH_ID=$1
+RULES_PATH=$2
+
+if [ "$SWITCH_ID" ] && [ "$RULES_PATH" ]; then
+    ./pox.py log.level --DEBUG log.color openflow.of_01 forwarding.l2_learning firewall --rules_path="$RULES_PATH" --switch_id="$SWITCH_ID"
+  else
+    ./pox.py log.level --DEBUG log.color openflow.of_01 forwarding.l2_learning firewall --rules_path="$RULES_PATH" --switch_id=1
+fi
