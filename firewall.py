f6b85b79e3b605ea286711e31feaba93d11f248f
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-11-12 21:26:55 -0300

    Agrego mas logs

--- firewall.py
+++ firewall.py
@@ -5,17 +5,24 @@ from pox.lib.util import dpidToStr
 from pox.lib.packet.ethernet import ethernet
 from pox.lib.packet.ipv4 import ipv4
 from pox.lib.addresses import EthAddr
-from rules_parser import parse_json
+import json
+
+def parse_json():
+    with open("rules.json") as data_file:
+        data = json.load(data_file)
+    return data
 
 log = core.getLogger()
+# Por defecto, tomamos que al switch al cual se le van a aplicar las reglas es el 1
 switch_id = 1
+
 
 class Firewall(EventMixin):
     def __init__(self):
         self.listenTo(core.openflow)
         log.debug("Enabling Firewall Module")
         self.rules = parse_json()
-        log.debug("Rules:\n" + str(self.rules))
+        log.debug("Rules loaded:\n" + str(self.rules))
 
     def _handle_ConnectionUp(self, event):
         # Instalamos el firewall en el primer switch
@@ -24,11 +31,14 @@ class Firewall(EventMixin):
                 if rule["name"] == "pto_1":
                     self.drop_packet_on_port(event, rule["protocol"], rule["dst_port"])
                 elif rule["name"] == "pto_2":
-                    self.drop_packet_on_port_and_ip_udp(event, rule["dst_port"], rule["src_ip"])
+                    self.drop_packet_on_port_and_ip_udp(
+                        event, rule["dst_port"], rule["src_ip"]
+                    )
                 elif rule["name"] == "pto_3":
-                    self.drop_packet_between_hosts(event, rule["src_mac"], rule["dst_mac"])
-
-            log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+                    self.drop_packet_between_hosts(
+                        event, rule["src_mac"], rule["dst_mac"]
+                    )
+            log.debug("Firewall rules installed on %s - switch %i", dpidToStr(event.dpid), switch_id)
 
     def drop_packet_on_port(self, event, protocol, dst_port):
         """
@@ -42,6 +52,7 @@ class Firewall(EventMixin):
         else:
             msg.match.nw_proto = ipv4.UDP_PROTOCOL
         event.connection.send(msg)
+        log.debug("Rule installed: dropping packets on dst_port %i and %s", dst_port, protocol.upper())
 
     def drop_packet_on_port_and_ip_udp(self, event, dst_port, src_ip):
         """
@@ -54,21 +65,23 @@ class Firewall(EventMixin):
         msg.match.nw_proto = ipv4.UDP_PROTOCOL
         msg.match.nw_src = src_ip
         event.connection.send(msg)
+        log.debug("Rule installed: dropping packets from host IP %s, dst_port %i and UDP", src_ip, dst_port)
 
     def drop_packet_between_hosts(self, event, src_mac, dst_mac):
         """
-        Se debe elegir dos hosts cualquiera, y los mismos no deben poder comunicarse de ninguna forma.        
+        Se debe elegir dos hosts cualquiera, y los mismos no deben poder comunicarse de ninguna forma.
         """
         msg = of.ofp_flow_mod()
         msg.match.dl_src = EthAddr(src_mac)
         msg.match.dl_dst = EthAddr(dst_mac)
         event.connection.send(msg)
+        log.debug("Rule installed: dropping packets from host MAC %s to host MAC %s", src_mac, dst_mac)
 
 
 def launch(s_id=1):
-    if (int(s_id) < 1):
+    if int(s_id) < 1:
         log.error("Invalid switch id")
         exit(-1)
     global switch_id
-    switch_id = int(s_id) 
+    switch_id = int(s_id)
     core.registerNew(Firewall)
--- rules_parser.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def parse_json():
-    with open('rules.json') as data_file:
-        data = json.load(data_file)
-    return data
